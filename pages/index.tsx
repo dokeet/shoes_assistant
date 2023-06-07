bf779dd8a72888954b5b2aacc81fc8c774d8bda6
angela <angela@incubator.com.ar>
2023-06-06 22:11:00 -0300

    add layout in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Layout from "@/components/Layout";
-import Carousel from "@/components/Carousels";
+import ProductsList from "@/components/ProductsList";
 import Chat from "@/components/Chat";
 import CarAi from "./car-ai";
 
 export default function Home() {
+  const [history, setHistory] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <div>
       <Head>
@@ -17,11 +20,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <CarAi />
-      {/* <Layout>
-        <Carousel />
-         <Chat /> 
-      </Layout>*/}
+      <Layout>
+        <ProductsList
+          setHistory={setHistory}
+          setLoading={setLoading}
+          history={history}
+        />
+        <Chat
+          setHistory={setHistory}
+          history={history}
+          setLoading={setLoading}
+          loading={loading}
+        />
+      </Layout>
     </div>
   );
 }
