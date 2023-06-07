dfc927b1e2a01cf7fc0aaf1302699a405e889ef2
angela <angela@incubator.com.ar>
2023-06-06 22:07:00 -0300

    change border of Card and swiper styles

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,29 +1,23 @@
 import React, { ReactNode } from "react";
 import { Card } from "../ui/card";
-import Image from "next/image";
 
 interface CardImagesProps {
   chunk: any;
   information: ReactNode;
+  children: ReactNode;
+  className: string;
 }
 
-export default function CardImages({ chunk, information }: CardImagesProps) {
+export default function CardImages({
+  chunk,
+  information,
+  children,
+  className,
+}: CardImagesProps) {
   return (
-    <Card key={chunk.id} className="flex flex-col max-w-[300px] ml-3">
+    <Card key={chunk.id} className={className}>
       <div className="h-auto overscroll-x-auto overflow-x-auto scroll-smooth overscroll-y-none flex snap-x rounded-t-md">
-        {JSON.parse(chunk?.images).map((image: string, i: number) => {
-          // console.log("image", image);
-          return (
-            <Image
-              key={`${image + i}`}
-              width={200}
-              height={200}
-              alt={`image of ${chunk.name}`}
-              src={image}
-              className="w-[300px] h-auto snap-center"
-            />
-          );
-        })}
+        {children}
       </div>
       {information}
     </Card>
