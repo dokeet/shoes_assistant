6c2fdbde20ae84eb07de72ae0b5cf04b0ac2f670
angela <angela@incubator.com.ar>
2023-06-06 22:03:23 -0300

    update tailwindcss and change slides in Answer

--- components/Answer/Answer.tsx
+++ components/Answer/Answer.tsx
@@ -3,12 +3,8 @@ import CardImages from "components/Card";
 import { Navigation, Pagination, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import Image from "next/image";
 import "swiper/css/navigation";
-import {
-  IconArrowRight,
-  IconExternalLink,
-  IconSearch,
-} from "@tabler/icons-react";
 
 interface AnswerProps {
   images: Array<any>;
@@ -18,39 +14,49 @@ interface AnswerProps {
 const AnswerComponent: React.FC<AnswerProps> = ({ images, answer }) => {
   // console.log("images", images);
   return (
-    <>
-      <div className="my-4">
-        <div className="border-blue-500 p-4 rounded-bl-2xl rounded-r-2xl border-2">
-          {answer}
-        </div>
-        <div className="w-full mt-4">
-          <Swiper
-            navigation={true}
-            modules={[Navigation]}
-            centeredSlides
-            slidesPerView={1}
-          >
-            {images?.map((chunk: any) => {
-              // console.log("chunk", chunk);
-              return (
-                <SwiperSlide key={chunk.id}>
-                  <CardImages
-                    chunk={chunk}
-                    information={
-                      <div>
-                        <h2 className="font-bold text-base mt-2 mb-2 px-2 max-h-16 text-ellipsis overflow-hidden whitespace-nowrap">
-                          {chunk.name}
-                        </h2>
-                      </div>
-                    }
-                  />
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
+    <div className="self-start max-w-xs mt-4 w-full">
+      <div className="shadow-md bg-gray-600 p-4 rounded-tl-2xl rounded-r-2xl font text-sm text-white">
+        {answer}
       </div>
-    </>
+      <div className="w-full mt-4 flex overflow-x-auto rounded-2xl">
+        {images?.map((chunk: any) => {
+          return (
+            <CardImages
+              key={chunk.id}
+              className="flex flex-col w-[230px] h-auto mr-2"
+              chunk={chunk}
+              information={
+                <div className="flex justify-center">
+                  <h2 className="font-bold text-base m-2 h-12">{chunk.name}</h2>
+                </div>
+              }
+            >
+              <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                centeredSlides
+                mousewheel={false}
+                slidesPerView={1}
+              >
+                {JSON.parse(chunk?.images).map((image: string, i: number) => {
+                  return (
+                    <SwiperSlide key={`${image + i}`}>
+                      <Image
+                        width={230}
+                        height={230}
+                        alt={`image of ${chunk.name}`}
+                        src={image}
+                        // className="w-[300px] h-auto snap-center"
+                      />
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
+            </CardImages>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
